61cbceedf52f694966bf283fe209c20a7876a849
Gonzalo Bahamondez <gbahamondezc@gmail.com>
2020-04-05 21:25:02 -0400

    Upgrading to terraform 0.12
    Using terraform `terraform 0.12upgrade` command to make configs 0.12 compatible

--- main.tf
+++ main.tf
@@ -1,50 +1,47 @@
 locals {
-  kubernetes_version = "${var.kubernetes_version != "latest" ? var.kubernetes_version : data.google_container_engine_versions.region.latest_node_version}"
+  kubernetes_version = var.kubernetes_version != "latest" ? var.kubernetes_version : data.google_container_engine_versions.region.latest_node_version
   nodes_network_tag  = "gke-node-${var.cluster_name}-network-tag"
 
-  nodes_locations = "${
-    length(var.nodes_locations) > 0
-    ? join(",", var.nodes_locations)
-    : join(",", data.google_compute_zones.available.names)
-  }"
+  nodes_locations = length(var.nodes_locations) > 0 ? join(",", var.nodes_locations) : join(",", data.google_compute_zones.available.names)
 
-  project = "${var.project == "" ? data.google_project.default.project_id : var.project}"
+  project = var.project == "" ? data.google_project.default.project_id : var.project
 }
 
-data google_project default {}
-
-data google_compute_zones available {
-  project = "${local.project}"
-  region  = "${var.region}"
+data "google_project" "default" {
 }
 
-data google_container_engine_versions region {
-  project  = "${local.project}"
-  location = "${data.google_compute_zones.available.names[0]}"
+data "google_compute_zones" "available" {
+  project = local.project
+  region  = var.region
 }
 
-resource google_container_cluster main {
-  provider                    = "google-beta"
+data "google_container_engine_versions" "region" {
+  project  = local.project
+  location = data.google_compute_zones.available.names[0]
+}
+
+resource "google_container_cluster" "main" {
+  provider                    = google-beta
   enable_binary_authorization = true
   initial_node_count          = 1
-  location                    = "${var.region}"
-  logging_service             = "${var.logging_service}"
-  min_master_version          = "${local.kubernetes_version}"
-  monitoring_service          = "${var.monitoring_service}"
-  name                        = "${var.cluster_name}"
-  network                     = "${var.network}"
-  node_locations              = ["${split(",", local.nodes_locations)}"]
-  project                     = "${local.project}"
+  location                    = var.region
+  logging_service             = var.logging_service
+  min_master_version          = local.kubernetes_version
+  monitoring_service          = var.monitoring_service
+  name                        = var.cluster_name
+  network                     = var.network
+  node_locations              = split(",", local.nodes_locations)
+  project                     = local.project
   remove_default_node_pool    = true
-  subnetwork                  = "${var.subnetwork}"
+  subnetwork                  = var.subnetwork
 
   addons_config {
     horizontal_pod_autoscaling {
-      disabled = "${!var.enable_horizontal_pod_autoscaling_addon}"
+      disabled = false == var.enable_horizontal_pod_autoscaling_addon
     }
 
     http_load_balancing {
-      disabled = "${!var.enable_http_load_balancing_addon}"
+      disabled = false == var.enable_http_load_balancing_addon
     }
 
     kubernetes_dashboard {
@@ -54,13 +51,13 @@ resource google_container_cluster main {
   }
 
   ip_allocation_policy {
-    cluster_secondary_range_name  = "${var.pods_secondary_ip_range_name}"
-    services_secondary_range_name = "${var.services_secondary_ip_range_name}"
+    cluster_secondary_range_name  = var.pods_secondary_ip_range_name
+    services_secondary_range_name = var.services_secondary_ip_range_name
   }
 
   maintenance_policy {
     daily_maintenance_window {
-      start_time = "${var.daily_maintenance_window}"
+      start_time = var.daily_maintenance_window
     }
   }
 
@@ -69,13 +66,24 @@ resource google_container_cluster main {
     password = ""
     username = ""
 
-    client_certificate_config = {
+    client_certificate_config {
       issue_client_certificate = false
     }
   }
 
   master_authorized_networks_config {
-    cidr_blocks = "${var.master_authorized_networks_config}"
+    dynamic "cidr_blocks" {
+      for_each = var.master_authorized_networks_config
+      content {
+        # TF-UPGRADE-TODO: The automatic upgrade tool can't predict
+        # which keys might be set in maps assigned here, so it has
+        # produced a comprehensive set here. Consider simplifying
+        # this after confirming which keys can be set in practice.
+
+        cidr_block   = cidr_blocks.value.cidr_block
+        display_name = lookup(cidr_blocks.value, "display_name", null)
+      }
+    }
   }
 
   network_policy {
@@ -86,7 +94,7 @@ resource google_container_cluster main {
   private_cluster_config {
     enable_private_endpoint = false
     enable_private_nodes    = true
-    master_ipv4_cidr_block  = "${var.master_ipv4_cidr_block}"
+    master_ipv4_cidr_block  = var.master_ipv4_cidr_block
   }
 
   timeouts {
@@ -96,17 +104,17 @@ resource google_container_cluster main {
   }
 }
 
-resource google_container_node_pool main {
-  count              = "${length(var.node_pools)}"
-  cluster            = "${google_container_cluster.main.name}"
-  initial_node_count = "${lookup(var.node_pools[count.index], "min_node_count", 1)}"
-  location           = "${var.region}"
-  name               = "${lookup(var.node_pools[count.index], "name")}"
-  project            = "${local.project}"
+resource "google_container_node_pool" "main" {
+  count              = length(var.node_pools)
+  cluster            = google_container_cluster.main.name
+  initial_node_count = lookup(var.node_pools[count.index], "min_node_count", 1)
+  location           = var.region
+  name               = var.node_pools[count.index]["name"]
+  project            = local.project
 
   autoscaling {
-    max_node_count = "${lookup(var.node_pools[count.index], "max_node_count", 10)}"
-    min_node_count = "${lookup(var.node_pools[count.index], "min_node_count", 1)}"
+    max_node_count = lookup(var.node_pools[count.index], "max_node_count", 10)
+    min_node_count = lookup(var.node_pools[count.index], "min_node_count", 1)
   }
 
   management {
@@ -115,13 +123,13 @@ resource google_container_node_pool main {
   }
 
   node_config {
-    disk_size_gb    = "${lookup(var.node_pools[count.index], "disk_size", 40)}"
-    disk_type       = "${lookup(var.node_pools[count.index], "disk_type", "pd-standard")}"
-    machine_type    = "${lookup(var.node_pools[count.index], "machine_type", "g1-small")}"
-    preemptible     = "${lookup(var.node_pools[count.index], "preemptible", false)}"
-    service_account = "${lookup(var.node_pools[count.index], "service_account", "")}"
+    disk_size_gb    = lookup(var.node_pools[count.index], "disk_size", 40)
+    disk_type       = lookup(var.node_pools[count.index], "disk_type", "pd-standard")
+    machine_type    = lookup(var.node_pools[count.index], "machine_type", "g1-small")
+    preemptible     = lookup(var.node_pools[count.index], "preemptible", false)
+    service_account = lookup(var.node_pools[count.index], "service_account", "")
 
-    metadata {
+    metadata = {
       disable-legacy-endpoints = "true"
     }
 
@@ -132,10 +140,19 @@ resource google_container_node_pool main {
       "https://www.googleapis.com/auth/monitoring",
     ]
 
-    tags = ["${local.nodes_network_tag}"]
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibility in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
+    tags = [local.nodes_network_tag]
   }
 
   lifecycle {
-    ignore_changes = ["initial_node_count"]
+    ignore_changes = [initial_node_count]
   }
 }
+
--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,20 @@
-output cluster_endpoint {
-  value       = "${google_container_cluster.main.endpoint}"
+output "cluster_endpoint" {
+  value       = google_container_cluster.main.endpoint
   description = "Cluster endpoint ip"
 }
 
-output cluster_name {
-  value       = "${var.cluster_name}"
+output "cluster_name" {
+  value       = var.cluster_name
   description = "Cluster name"
 }
 
-output master_auth_cluster_ca_certificate {
-  value       = "${google_container_cluster.main.master_auth.0.cluster_ca_certificate}"
+output "master_auth_cluster_ca_certificate" {
+  value       = google_container_cluster.main.master_auth[0].cluster_ca_certificate
   description = "Master CA certificate"
 }
 
-output nodes_network_tag {
-  value       = "${local.nodes_network_tag}"
+output "nodes_network_tag" {
+  value       = local.nodes_network_tag
   description = "Network tag that will be applied to every node"
 }
+
