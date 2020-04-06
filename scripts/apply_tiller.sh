98d787949e0981080b15c6967f3763d6142f0ef2
Gonzalo Bahamondez <gbahamondezc@gmail.com>
2020-04-06 12:47:49 -0400

    Debug

--- scripts/apply_tiller.sh
+++ scripts/apply_tiller.sh
@@ -5,6 +5,9 @@ set -xeuo pipefail
 source ./wait_cluster.sh
 source ./kubectl_auth.sh
 
+echo "debugging"
+cat ./tiller_service_account.yml
+
 cat ./tiller_service_account.yml | kubectl create -f -
 
 helm init --service-account tiller
